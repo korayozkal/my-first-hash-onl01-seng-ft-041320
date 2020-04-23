35bea6b8e56f58efe06e1d45affa5e38bd6b7966
Koray Ozkal <koryozkal@gmail.com>
2020-04-23 01:32:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,10 +35,10 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest
 
 
   # return the shipping_manifest hash below
